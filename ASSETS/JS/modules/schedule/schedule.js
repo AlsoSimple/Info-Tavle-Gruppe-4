8a00eaabc4de027034b073a611fc58c50022fac4
ClasikDK <philipdethlefsen251@gmail.com>
2024-10-08 11:06:42 +0200

    Schedule Virker

--- ASSETS/JS/modules/schedule/schedule.js
+++ ASSETS/JS/modules/schedule/schedule.js
@@ -1,5 +1,5 @@
 export default function schedule(schedules) {
-    console.log(schedules);
+    let currentTime = new Date("2024-10-07T09:00:00+02:00");
 
     let currentTime = new Date();
 
@@ -14,15 +14,62 @@ export default function schedule(schedules) {
             schedule.Team === "h0mg010124f"
     );
 
-    console.log(filteredSchedules);
-
-    let upcomingSchedules = filteredSchedules.filter(
-        (schedule) => currentTime > new Date(schedule.StartDate)
-    );
-
-    upcomingSchedules = upcomingSchedules.sort(
-        (a, b) => new Date(a.StartDate) - new Date(b.StartDate)
-    );
+    const upcomingSchedules = filteredSchedules
+        .filter((schedule) => {
+            let startDate = new Date(schedule.StartDate);
+            return (
+                currentTime < startDate &&
+                getDayOfWeek(startDate) === getDayOfWeek(currentTime)
+            );
+        })
+        .sort((a, b) => new Date(a.StartDate) - new Date(b.StartDate));
 
     console.log(upcomingSchedules);
+    makeSchedule(upcomingSchedules);
+}
+
+function getDayOfWeek(date) {
+    const daysOfWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    return daysOfWeek[date.getDay()];
+}
+
+function makeSchedule(schedule) {
+    const scheduleElement = document.getElementById("skema");
+    let html = `
+        <h1>HER FINDER DU OS</h1>
+        <hgroup>
+          <h3>Lokale</h3>
+          <h3>Holdnavn</h3>
+          <h3>Fag</h3>
+          <h3>Uddannelse</h3>
+          <h3>Tid</h3>
+        </hgroup>
+    `;
+    for (let i = 0; i < 11; i++) {
+        let startDate = new Date(schedule[i].StartDate);
+
+        const startTime = startDate.getHours() + ":" + startDate.getMinutes();
+
+        html += `
+            <div>
+                <hgroup>
+                  <h3>${schedule[i].Room}</h3>
+                  <h3>${schedule[i].Team}</h3>
+                  <h3>${schedule[i].Subject}</h3>
+                  <h3>${schedule[i].Education}</h3>
+                  <h3>${startTime}</h3>
+                </hgroup>
+            </div>
+        `;
+    }
+
+    scheduleElement.innerHTML += html;
 }
